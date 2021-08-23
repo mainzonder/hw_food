890a5fcb2969bd9098f7163a1a446adb53031170
mainzonder <no7au53r@gmail.com>
2021-08-23 18:24:01 +0300

    regular + refactoring

--- js/script.js
+++ js/script.js
@@ -391,10 +391,10 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
   next.addEventListener('click', () => {
-    if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+    if (offset == +width.replace(/\D/g, '') * (slides.length - 1)) {
       offset = 0;
     } else {
-      offset += +width.slice(0, width.length - 2);
+      offset += +width.replace(/\D/g, '')
     }
     slidesField.style.transform = `translateX(-${offset}px)`;
 
@@ -417,9 +417,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
   prev.addEventListener('click', () => {
     if (offset == 0) {
-      offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+      offset = +width.replace(/\D/g, '') * (slides.length - 1);
     } else {
-      offset -= +width.slice(0, width.length - 2);
+      offset -= +width.replace(/\D/g, '')
     }
 
     slidesField.style.transform = `translateX(-${offset}px)`;
@@ -444,7 +444,7 @@ window.addEventListener('DOMContentLoaded', () => {
       const slideTo = e.target.getAttribute('data-slide-to');
 
       slideIndex = slideTo;
-      offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+      offset = +width.replace(/\D/g, '') * (slideTo - 1);
 
       slidesField.style.transform = `translateX(-${offset}px)`;
 
